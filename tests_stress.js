ee359b0b75608fca3090e68e374ba0871406f5dd
Soledad Penades <sole@mozilla.com>
2014-01-08 16:48:32 +0000

    tests_stress a bit better-actually capturing things, but why does it stop?

--- tests_stress.js
+++ tests_stress.js
@@ -3,38 +3,36 @@ window.onload = function() {
     var statusDiv = document.getElementById('status');
     var imagesDiv = document.getElementById('images');
     var canvasAnimation = document.getElementById('animation');
+    var width = canvasAnimation.offsetWidth;
+    var height = canvasAnimation.offsetHeight;
+
     var ctxAnimation = canvasAnimation.getContext('2d');
     var iterations = 0;
 
     captureGIF();
 
     function captureGIF() {
+
         iterations++;
         setStatus('capturing GIF ' + Date.now() + ' ' + iterations);
 
-        var width = cssAnimation.offsetWidth;
-        var height = cssAnimation.offsetHeight;
-
-        var captureCanvas = document.createElement('canvas');
-        var ctx = captureCanvas.getContext('2d');
         
-        captureCanvas.width = width;
-        captureCanvas.height = height;
+        var color = (((iterations * 0.01) * 0xFFFFFF) | 0) % 0xFFFFFF;
+        console.log('color', color);
 
-        ctx.drawImage(cssAnimation, 0, 0);
+        ctxAnimation.fillStyle = '#' + color;
+        ctxAnimation.fillRect(0, 0, width, height);
 
         var ag = new Animated_GIF({ repeat: null, workerPath: 'dist/Animated_GIF.worker.js' }); // Don't repeat
         ag.setSize(width, height);
-        ag.addFrame(captureCanvas);
+        ag.addFrame(canvasAnimation);
 
         ag.getBase64GIF(function(gif) {
             var img = document.createElement('img');
             img.src = gif;
             imagesDiv.appendChild(img);
-            setTimeout(captureGIF, 1000);
+            setTimeout(captureGIF, 100);
         });
-
-
 
 
     }
