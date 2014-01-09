92e2ffeecb79fd74428cce606333507b6257aaf4
Soledad Penades <sole@mozilla.com>
2014-01-09 11:03:48 +0000

    showing elapsed time in the canvasses

--- tests_stress.js
+++ tests_stress.js
@@ -5,25 +5,57 @@ window.onload = function() {
     var canvasAnimation = document.getElementById('animation');
     var width = canvasAnimation.offsetWidth;
     var height = canvasAnimation.offsetHeight;
+    var lastTraceLogTimestamp = 0;
+    var lastCaptureTime = 0;
 
     var ctxAnimation = canvasAnimation.getContext('2d');
+    var textColors = ['black', 'white'];
     var iterations = 0;
+
+    ctxAnimation.textAlign = 'center';
+    ctxAnimation.textBaseline = 'middle';
+    ctxAnimation.font = '12px arial';
 
     captureGIF();
 
     function captureGIF() {
 
-        iterations++;
-        setStatus('capturing GIF ' + Date.now() + ' ' + iterations);
+        var now = Date.now();
 
+        if(iterations >= 50) {
+            return;
+        }
+
+        setStatus('capturing GIF ' + now + ' iteration = ' + iterations);
+
+        var elapsed = lastCaptureTime > 0 ? ((now - lastCaptureTime) / 1000).toFixed(2) : 0;
+        lastCaptureTime = now;
+
+        var color;
         
-        var color = (((iterations * 0.01) * 0xFFFFFF) | 0) % 0xFFFFFF;
-        console.log('color', color);
+        if(elapsed > 1) { 
+            color = 'red';
+            console.log('TOO SLOW! on the ' + iterations + ' == ' + elapsed + ' seconds');
+        } else {
+            color = ( (Math.random() * 0xFFFF) | 0).toString(16);
+            while(color.length < 6) {
+                color = '0' + color;
+            }
+            color = '#' + color;
+        }
 
-        ctxAnimation.fillStyle = '#' + color;
+        // Draw in the canvas
+        ctxAnimation.fillStyle = color;
         ctxAnimation.fillRect(0, 0, width, height);
 
-        var ag = new Animated_GIF({ repeat: null, workerPath: 'dist/Animated_GIF.worker.js' }); // Don't repeat
+        textColors.forEach(function(txtColor, offset) {
+            ctxAnimation.fillStyle = txtColor;
+            ctxAnimation.fillText(elapsed, width / 2 - offset, height / 2 - offset);
+        });
+
+
+        // Make a one-frame GIF
+        var ag = new Animated_GIF({ repeat: null, workerPath: 'dist/Animated_GIF.worker.js' });
         ag.setSize(width, height);
         ag.addFrame(canvasAnimation);
 
@@ -31,13 +63,15 @@ window.onload = function() {
             var img = document.createElement('img');
             img.src = gif;
             imagesDiv.appendChild(img);
-            setTimeout(captureGIF, 100);
+            setTimeout(captureGIF, 10);
         });
 
+        iterations++;
 
     }
 
     function setStatus(msg) {
         statusDiv.innerHTML = msg;
     }
+
 };
