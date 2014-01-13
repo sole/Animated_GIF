312b6f02c57ce8623f0ca212b5813ab6f7d02532
Soledad Penades <sole@mozilla.com>
2014-01-13 00:14:04 +0000

    Renamed files to make more sense

--- src/build.sh
+++ src/build.sh
@@ -1,4 +1,4 @@
-cat NeuQuant.js quantizer.js > ../dist/Animated_GIF.worker.js
+cat NeuQuant.js Animated_GIF.worker.js > ../dist/Animated_GIF.worker.js
 browserify main.js > ../dist/Animated_GIF.js
 
 # minified versions too
